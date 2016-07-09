8187c8627774476c926758ab678e3c1f545bc79e
sharminnie0786 <sharmynkayani1@gmail.com>
2016-07-09 20:09:58 +1000

    Add files via upload

---
+++ teenSum.cpp
@@ -0,0 +1,27 @@
+//
+//  teenSum.cpp
+//  PracticeQuestions3
+//
+//  Created by Sharmyn Kayani on 9/07/2016.
+//  Copyright © 2016 Sharmyn Kayani. All rights reserved.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//int teenSum(int a, int b) {
+//    int sum = a + b;
+//    
+//    if (a >= 13 || a <= 19) {
+//        return 19;
+//    } else if (b >= 13 || b <= 19) {
+//        return 19;
+//    } else {
+//        return sum;
+//    }
+//}
+//
+//int main() {
+//    
+//    cout << teenSum(13, 2) << endl;
+//}
