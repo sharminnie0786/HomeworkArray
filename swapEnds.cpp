ef246c3358efed2ee70d7e920b6cbc2e77eb1c85
sharminnie0786 <sharmynkayani1@gmail.com>
2016-07-10 14:27:46 +1000

    Add files via upload

---
+++ swapEnds.cpp
@@ -0,0 +1,31 @@
+//
+//  swapEnds.cpp
+//  PracticeQuestions3
+//
+//  Created by Sharmyn Kayani on 9/07/2016.
+//  Copyright © 2016 Sharmyn Kayani. All rights reserved.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int arr[6];
+//    srand(time(0));
+//    int temp, newfirst, newsecond = 0;
+//    
+//    for (int i = 0; i < 6; i++) {
+//        arr[i] = rand() % 10;
+//        cout << arr[i] << " ";
+//    }
+//    
+//    cout << endl;
+//        temp = arr[0];
+//        arr[0] = arr[5];
+//        arr[5] = temp;
+//        newfirst = arr[5];
+//        newsecond = arr[0];
+//    
+//    cout << newsecond << " " << newfirst << endl;
+//    return 0;
+//}
